a7ae8ed3091442bb20b4f6842dfebebca8d9b932
unknown-user <josip.bakic@gmail.com>
2013-05-06 00:37:00 +0200

    Comment update.

--- Trans/IShielded.cs
+++ Trans/IShielded.cs
@@ -5,12 +5,9 @@ namespace Trans
     internal interface IShielded
     {
         bool HasChanges { get; }
-        // once having responded to this, implementor must ensure answer stays
-        // the same until Commit() or Rollback(). other threads asking to read
-        // will get immediate response if their reading stamp is less
-        // then this one here. if it is greater, callers spin wait until that
-        // Commit() or Rollback() from before happens. calls to CanCommit
-        // always spin wait, their stamp is always larger.
+        // this locks the implementor. All reads with >stamp are
+        // spinwaited, all other threads' CanCommits() return false,
+        // and only a Commit() or Rollback(with the same stamp) release it.
         bool CanCommit(bool strict, long writeStamp);
         bool Commit(long writeStamp);
         void Rollback(long? writeStamp = null);
