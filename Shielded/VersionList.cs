e372571b96c2adf98c5bbd37e7cba23e5278d664
unknown-user <josip.bakic@gmail.com>
2013-09-03 22:28:59 +0200

    VersionList - Using ordinary lock statements, same speed but much simpler.

--- Shielded/VersionList.cs
+++ Shielded/VersionList.cs
@@ -5,24 +5,20 @@ using System.Linq;
 
 namespace Shielded
 {
-    public class VersionList
+    internal class VersionList
     {
         private Dictionary<long, int> _items = new Dictionary<long, int>();
         private long? _min;
-        private int _busy = 0;
+        private object _lock = new object();
 
         public long SafeAdd(Func<long> generator)
         {
             if (generator == null)
                 throw new ArgumentNullException();
 
-            long i;
-            bool gotBusy = false;
-            try
+            lock (_lock)
             {
-                SpinWait.SpinUntil(() =>
-                    gotBusy = Interlocked.CompareExchange(ref _busy, 1, 0) == 0);
-                i = generator();
+                var i = generator();
                 if (_items.ContainsKey(i))
                     _items [i] = _items [i] + 1;
                 else
@@ -30,21 +26,14 @@ namespace Shielded
                     _items.Add(i, 1);
                     if (_min == null || _min > i) _min = i;
                 }
+                return i;
             }
-            finally
-            {
-                if (gotBusy) Interlocked.Exchange(ref _busy, 0);
-            }
-            return i;
         }
 
         public void Remove(long i)
         {
-            bool gotBusy = false;
-            try
+            lock (_lock)
             {
-                SpinWait.SpinUntil(() =>
-                    gotBusy = Interlocked.CompareExchange(ref _busy, 1, 0) == 0);
                 if (_items.ContainsKey(i))
                 {
                     if (_items [i] > 1)
@@ -57,25 +46,14 @@ namespace Shielded
                     }
                 }
             }
-            finally
-            {
-                if (gotBusy) Interlocked.Exchange(ref _busy, 0);
-            }
         }
 
         public long? Min()
         {
-            bool gotBusy = false;
-            try
+            lock (_lock)
             {
-                SpinWait.SpinUntil(() =>
-                    gotBusy = Interlocked.CompareExchange(ref _busy, 1, 0) == 0);
                 //return _items.Any() ? _items.First().Key : (long?)null;
                 return _min;
-            }
-            finally
-            {
-                if (gotBusy) Interlocked.Exchange(ref _busy, 0);
             }
         }
     }
