e44d3c77ee7a00721deca290b4daf4ec3e0b0a7a
jbakic <josip.bakic@gmail.com>
2015-02-15 01:03:03 +0100

    Shielded<T>.Changed created only when needed.

--- Shielded/Shielded.cs
+++ Shielded/Shielded.cs
@@ -185,7 +185,16 @@ namespace Shielded
         /// after the commute is enlisted, and your handler can easily cause commutes to
         /// degenerate.
         /// </summary>
-        public readonly ShieldedEvent<EventArgs> Changed = new ShieldedEvent<EventArgs>();
+        public ShieldedEvent<EventArgs> Changed
+        {
+            get
+            {
+                if (_changed == null)
+                    Interlocked.CompareExchange(ref _changed, new ShieldedEvent<EventArgs>(), null);
+                return _changed;
+            }
+        }
+        private ShieldedEvent<EventArgs> _changed;
 
         public static implicit operator T(Shielded<T> obj)
         {
