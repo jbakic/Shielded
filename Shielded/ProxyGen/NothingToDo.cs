4c95d11e1837e02c586cdf176d310bc9c6af974f
jbakic <josip.bakic@gmail.com>
2015-11-07 20:35:10 +0100

    Minor fix in ProxyGen

--- Shielded/ProxyGen/NothingToDo.cs
+++ Shielded/ProxyGen/NothingToDo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel;
+using System.Reflection;
 
 namespace Shielded.ProxyGen
 {
@@ -12,7 +13,8 @@ namespace Shielded.ProxyGen
         {
             return !t.IsClass || t.IsSealed || !t.IsPublic ||
                 t.GetConstructor(Type.EmptyTypes) == null ||
-                !t.GetProperties().Any(ProxyGen.IsInteresting);
+                !t.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                    .Any(ProxyGen.IsInteresting);
         }
     }
 }
