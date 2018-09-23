394be8276bb4ed648b82b172aeb9864566a31f49
Josip Bakic <josip.bakic@gmail.com>
2018-09-23 16:15:01 +0200

    Add PreCommitFirstCommute test
    This was forgotten when porting changes from
    Shielded.Gossip over to this repo. The fix is already here tho.

--- ShieldedTests/PreCommitTests.cs
+++ ShieldedTests/PreCommitTests.cs
@@ -229,6 +229,17 @@ namespace ShieldedTests
             Assert.AreEqual(0, failCommitCount);
             Assert.AreEqual(0, failVisibleCount);
         }
+
+        [Test]
+        public void PreCommitFirstCommute()
+        {
+            var a = new Shielded<int>();
+            var b = new Shielded<int>();
+            using (Shield.PreCommit(() => a == 0 || true, () => b.Commute((ref int n) => n++)))
+                Shield.InTransaction(() => a.Value = 1);
+            Assert.AreEqual(1, a);
+            Assert.AreEqual(1, b);
+        }
     }
 }
 
